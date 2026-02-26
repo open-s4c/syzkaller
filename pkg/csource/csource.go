6e90fbb420cf012e05875f7350244392bc3b5a5d
martin-beck <martin.beck2@gmx.de>
2026-02-26 11:15:48 +0100

    Update pkg/csource/csource.go
    Co-authored-by: Lilith Oberhauser <lilith.oberhauser@huawei.com>
    Signed-off-by: martin-beck <martin.beck2@gmx.de>

--- pkg/csource/csource.go
+++ pkg/csource/csource.go
@@ -580,16 +580,7 @@ func (ctx *context) fmtCallBody(call prog.ExecCall) string {
 			}
 			if i == 1 {
 				switch callName {
-				case "write":
-					argsStrs = append(argsStrs, "UNIQUE_VAR(ctx->writeBuffer)")
-					continue
-				case "pwrite64":
-					argsStrs = append(argsStrs, "UNIQUE_VAR(ctx->writeBuffer)")
-					continue
-				case "send":
-					argsStrs = append(argsStrs, "UNIQUE_VAR(ctx->writeBuffer)")
-					continue
-				case "sendto":
+				case "write", "pwrite64",  "send",  "sendto":
 					argsStrs = append(argsStrs, "UNIQUE_VAR(ctx->writeBuffer)")
 					continue
 				}
